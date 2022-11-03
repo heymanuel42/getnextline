e4707c89a098015ee59d605e9d643b9e38268698
ejanssen <ejanssen@student.42lausanne.ch>
2022-11-03 18:04:15 +0100

    resolved timeout

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ejanssen <ejanssen@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 14:01:10 by ejanssen          #+#    #+#             */
-/*   Updated: 2022/11/03 17:59:54 by ejanssen         ###   ########.fr       */
+/*   Updated: 2022/11/03 18:00:56 by ejanssen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(void)
 {
 	int	fd;
 
-	fd = open("empty.txt", O_RDONLY);
+	fd = open("test.txt", O_RDONLY);
 	ft_read(fd);
 	ft_read(fd);
 	close(fd);
